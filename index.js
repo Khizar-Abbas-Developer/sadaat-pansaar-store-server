69019f65a913aca2483491494e0df95ba99a89c4
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-04-18 20:40:14 +0500

    critical

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import compression from "compression";
+
+import connectDB from "./config/db.js";
+
+// Initialize app
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(compression());
+app.use(express.json());
+
+// CORS config
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://sadaat-pansaar-store-server.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`✅ Server running at http://localhost:${port}`);
+});
